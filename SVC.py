d9b58ffdc9f4ee385b06de429756941076f89e28
frenkvdberg <49718880+frenkvdberg@users.noreply.github.com>
2021-10-31 18:40:25 +0100

    SVC updated version

--- SVC.py
+++ SVC.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python
 
 """
-# run as python SVC -i train
-(where train is a directory containing COP1-COP22 files)
+Combines an SVM model (SVC or LinearSVC) with a TF-IDF vectorizer to
+perform our binary classification task. Different parameter settings can
+be specified with command line arguments. Also, optionally, the N most informative
+features can be printed.
+Authors: Esther Ploeger, Frank van den Berg
 """
 
 import argparse
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.model_selection import cross_val_predict, cross_val_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
-from sklearn.svm import LinearSVC, SVR, SVC
-from os import listdir  # to read files
-from os.path import isfile, join  # to read files
-import matplotlib.pyplot as plt
-import sys
-import warnings
-import os
-import json
-import re
+from sklearn.svm import LinearSVC, SVC
 from collections import Counter
-from nltk import sent_tokenize, word_tokenize, pos_tag
-from sklearn import metrics
+import sys
 import time
 import preprocessing
 import pandas as pd
@@ -39,18 +32,22 @@ def create_arg_parser():
     parser.add_argument("-k", "--kernel", default='rbf', type=str,
                         help="The desired kernel when using the SVC or SVR model:"
                              " 'linear' or 'rbf' (default 'rbf')")
-    parser.add_argument("-l", "--loss", default='hinge', type=str,
+    parser.add_argument("-l", "--loss", default='squared_hinge', type=str,
                         help="The desired loss function: "
-                             "'hinge' or 'squared_hinge' (default 'hinge')")
+                             "'hinge' or 'squared_hinge' (default 'squared_hinge')")
     parser.add_argument("-c", "--c_parameter", default=1.0, type=float,
                         help="The value of the SVM's regularization parameter C (default 1.0)")
-    parser.add_argument("-g", "--gamma", default=1.0, type=float,
-                        help="The value of the SVM's gamma parameter (default 1.0)")
     parser.add_argument("-n", "--n_gram", default=2, type=int,
-                        help="The desired number of N-grams that is given to the tfidf-"
+                        help="The desired number of N-grams that is given to the TFIDF-"
                              "vectorizer in the parameter ngram_range(1, n) (default=2)")
-    parser.add_argument("-coef", "--coefficients", action="store_true",
-                        help="Print the weights that are assigned to each feature when kernel=linear")
+    parser.add_argument("-min", "--min_df", default=0.01, type=float,
+                        help="The desired value for the min_df parameter that is given to the TFIDF-"
+                             "vectorizer (default=0.01)")
+    parser.add_argument("-max", "--max_df", default=0.32, type=float,
+                        help="The desired value for the max_df parameter that is given to the TFIDF-"
+                             "vectorizer (default=0.32)")
+    parser.add_argument("-mi", "--most_informative", default=0, type=int,
+                        help="Print the N most informative features (default 0")
 
     args = parser.parse_args()
     return args
@@ -61,23 +58,28 @@ def identity(x):
     return x
 
 
-def choose_model(model, c, loss, kernel, gamma):
+def choose_model(model, c, loss, kernel):
     """Returns the right model with the specified parameter values"""
     models = {"LinearSVC": LinearSVC(random_state=0, C=c, loss=loss, class_weight='balanced'),
-              "SVR": SVR(kernel=kernel, C=c, gamma=gamma),
               "SVC": SVC(kernel=kernel, C=c, class_weight='balanced')}
 
     return models[model]
 
 
-def pos(txt):
-    return [p for p in pos_tag(txt)]
+def print_info(model, kernel, testfile, loss, vectorizer="TF-IDF"):
+    """Prints information about the test file, the chosen model and parameter values"""
+    print("\nTest file is: {}".format(testfile))
+    print("Model: {}".format(model))
+    print("Vectorizer: {} vectorizer".format(vectorizer))
+    if model == "SVC":
+        print("Kernel: {}".format(kernel))
+    print("Loss: {}\n".format(loss))
 
 
 def show_most_informative(feature_names, coefs, n=20):
+    """Prints the N most informative features"""
     # Zip coefficients and names together and make a DataFrame
-    zipped = zip(feature_names, coefs)
-    df = pd.DataFrame(zipped, columns=["feature", "value"])
+    df = pd.DataFrame(zip(feature_names, coefs), columns=["feature", "value"])
     # Sort the features by the absolute value of their coefficient
     df["abs_value"] = df["value"].apply(lambda x: abs(x))
     df["colors"] = df["value"].apply(lambda x: "green" if x > 0 else "red")
@@ -85,57 +87,49 @@ def show_most_informative(feature_names, coefs, n=20):
 
     print(df.head(n))
 
+def create_train_test(train_dir, testfile):
+    """Takes all the train files and the test file create
+    train and test data containing articles and labels"""
+    X_train, Y_train = [], []
+    train_filenames = preprocessing.get_filenames_in_folder(train_dir)
+    for fn in train_filenames:
+        texts, labels = preprocessing.read_corpus(train_dir + '/' + fn)
+        X_train = X_train + texts
+        Y_train = Y_train + labels
+    X_test, Y_test = preprocessing.read_corpus(testfile)
+
+    print("# Division of labels\n\tTrain: {}\n\tTest: {}".format(Counter(Y_train), Counter(Y_test)))
+    return X_train, Y_train, X_test, Y_test
+
 
 if __name__ == "__main__":
     args = create_arg_parser()
 
-    # Get filenames from input (training) directory
-    filenames = preprocessing.get_filenames_in_folder(args.input_dir)
-
     # Create train and test set
     print("## Preprocessing...", file=sys.stderr)
-    X_test, Y_test = preprocessing.read_corpus(args.test_file)
-    X_train, Y_train = [], []
-    for fn in filenames:
-        texts, labels = preprocessing.read_corpus(args.input_dir + '/' + fn)
-        X_train = X_train + texts
-        Y_train = Y_train + labels
+    X_train, Y_train, X_test, Y_test = create_train_test(args.input_dir, args.test_file)
 
-    print(Counter(Y_train))
-    print(Counter(Y_test))
-
-    c = 1.0
-    vec = TfidfVectorizer(tokenizer=identity, preprocessor=identity, ngram_range=(1,2))
-    #pos = TfidfVectorizer(tokenizer=pos, preprocessor=identity)
-    # union = FeatureUnion([("vec", vec), ("pos", pos)])
-
-    # Get the right model with the specified parameter values
-    svm_model = choose_model(args.model, c, args.loss, args.kernel, args.gamma)
-    # clf = Pipeline([('union', union), ('cls', svm_model)])
+    # Combine vectorizer with the SVM model and print the experiment setup information
+    vec = TfidfVectorizer(tokenizer=identity, preprocessor=identity,
+                          ngram_range=(1,args.n_gram), min_df=args.min_df, max_df=args.max_df)
+    svm_model = choose_model(args.model, args.c_parameter, args.loss, args.kernel)
     clf = Pipeline([('vec', vec), ('cls', svm_model)])
+    print_info(args.model, args.kernel, args.test_file, args.loss)
 
-    # Predict the tags for the reviews using either cross validation or the regular train and test data:
+    # Predict the political orientation for the articles
+    print("## Predicting...", file=sys.stderr)
     t0 = time.time()
     clf.fit(X_train, Y_train)
-
     Y_pred = clf.predict(X_test)
+
     # Report the precision, recall, f1-score and accuracy scores along with the runtime
     runtime = time.time() - t0
     print(classification_report(Y_test, Y_pred, digits=3))
-    print("Completed in {:.1f} seconds".format(runtime))
+    print("Completed in {:.1f} seconds\n\n".format(runtime))
 
-    # Show N most informative features:
-    feature_names = clf.named_steps["vec"].get_feature_names()
-    coefs = clf.named_steps["cls"].coef_.flatten()  # Get the coefficients of each feature
-    """"
-    vf = (clf
-                  .named_steps["union"]
-                  .transformer_list[0][1]
-                  .get_feature_names())
-    pf = (clf
-               .named_steps["union"]
-               .transformer_list[1][1]
-               .get_feature_names())
-    feature_names = vf + pf
-    """
-    show_most_informative(feature_names, coefs, 50)
+    # Optionally, show N most informative features:
+    if args.most_informative > 0:
+        # Get names and coefficients for each feature, then show most informative features
+        feature_names = clf.named_steps["vec"].get_feature_names()
+        coefs = clf.named_steps["cls"].coef_.flatten()
+        show_most_informative(feature_names, coefs, args.mi)
